13c8545b75bae69a67fdbaf95f2fda9ed70f63d1
Arkadiusz Wiśniewski <darek253633@gmail.com>
2023-10-04 01:47:26 +0200

    Install clsx

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
